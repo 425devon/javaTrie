71075f0d2d71f83266ec8ef28f094c04d6b00461
425devon <425devon@gmail.com>
2018-07-25 13:14:30 -0700

    added Trie Node class

---
+++ src/com/company/Node.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Node {
+    public HashMap<Character, Node> children;
+    public boolean isCompleted;
+
+    public Node(){
+        this.children = new HashMap<Character, Node>();
+        this.isCompleted = false;
+    }
+}
