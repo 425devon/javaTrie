0a5090bf44672a9d326520acbd0c6018cc8ac7aa
425devon <425devon@gmail.com>
2018-07-25 13:15:08 -0700

    initialized project

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Trie testTrie = new Trie();
+
+	    testTrie.insert("chat");
+	    testTrie.insert("chart");
+	    testTrie.insert("chipper");
+	    testTrie.insert("skipper");
+
+    }
+}
